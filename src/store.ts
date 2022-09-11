5093d85c2b2436aca5b03c24c709e18b6bdad93a
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-09-11 17:32:54 +0200

    #1 v1.1

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import contentV10  from './manifests/v1.0.html?raw'
-//import contentV11  from './manifests/v1.1.html?raw'
+import contentV11  from './manifests/v1.1.html?raw'
 
 export type Manifest = {
     version: string
@@ -12,14 +12,12 @@ export type Manifest = {
  * Note: manifests must be sorted descending to keep the functionality.
  */
 const manifests = <Manifest[]>[
-    /*
     {
         version: 'v1.1',
         content: contentV11,
         inception: '2022-08-25',
         wallet: '0x475f913Cc75F89DDAb521c717111F39778BA770b',
     },
-    */
     {
         version: 'v1.0',
         content: contentV10,
